b850066b3ddca69d10e4893d194a31096f72d566
nahid-sheikh <nahidsheikh.shariar.com>
2021-04-29 18:14:23 +0600

    added env file and redeploy to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,5 @@
 import firebase from 'firebase';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBH0Z6F6m736EPpIL6zKedOtW4E9R3iH4s",
-//   authDomain: "linkedin-clone-try.firebaseapp.com",
-//   projectId: "linkedin-clone-try",
-//   storageBucket: "linkedin-clone-try.appspot.com",
-//   messagingSenderId: "696780809300",
-//   appId: "1:696780809300:web:393e43e13c7271c555b9c1",
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
